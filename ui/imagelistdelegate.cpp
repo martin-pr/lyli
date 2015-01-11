f824a06b69453230c9e2388edb266f9de1acf7bb
Lukas Jirkovsky <l.jirkovsky@gmail.com>
2015-01-12 00:06:25 +0100

    ui: fix the look of items in the image list.

--- ui/imagelistdelegate.cpp
+++ ui/imagelistdelegate.cpp
@@ -1,5 +1,6 @@
 #include "imagelistdelegate.h"
 
+#include <QtCore/QRect>
 #include <QtGui/QPainter>
 
 #include "imagelistitem.h"
@@ -31,20 +32,27 @@ void ImageListDelegate::paint(QPainter* painter, const QStyleOptionViewItem& opt
 	}
 	
 	painter->setPen(Qt::black);
-	painter->drawRect(option.rect);
+	// the rectangle in which we have to draw and the upper left
+	// corner that has to be used to offset into its position
+	QRect rect(option.rect.topLeft() + QPoint(8,8), option.rect.bottomRight() - QPoint(8,8));
+	QPoint ul(rect.topLeft());
 	
+	// drawing
+	
+	// border
+	painter->drawRect(rect);
 	// draw the thumbnail
-	painter->drawImage(QPoint(8, 8), item.getImage());
+	painter->drawImage(ul + QPoint(8, 8), item.getImage());
 	// draw the date
-	painter->drawText(QPoint(0, 142), item.getTime().toString("d MMM yyyy"));
+	painter->drawText(ul + QPoint(8, 148), item.getTime().toString("d MMM yyyy"));
 	// draw the time
-	painter->drawText(QPoint(0, 162), item.getTime().toString("hh:mm:ss"));
+	painter->drawText(ul + QPoint(8, 168), item.getTime().toString("hh:mm:ss"));
 	
 	painter->restore();
 }
 
 QSize ImageListDelegate::sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const
 {
-	return QSize(144, 184);
+	return QSize(160, 200);
 }
 
