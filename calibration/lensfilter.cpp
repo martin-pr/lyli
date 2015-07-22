e480df2fc48455eb066bfd023d86c53f95bbcc7a
Lukas Jirkovsky <l.jirkovsky@gmail.com>
2015-07-22 20:51:48 +0200

    cleanup.

--- calibration/lensfilter.cpp
+++ calibration/lensfilter.cpp
@@ -25,55 +25,93 @@
 #endif
 
 using LinePtrList = std::vector<Lyli::Calibration::Line*>;
+using DPair = std::pair<double, double>;
 
-namespace Lyli {
-namespace Calibration {
+namespace {
 
-LineMap LensFilter::filter(const LineMap& lines) {
-#ifndef NDEBUG
-	struct {
-		std::size_t shortlines = 0;
-		std::size_t boundary = 0;
-	} stats;
-#endif
+/**
+ * Compute the average line length.
+ *
+ * \param lines lines for which the average is computed
+ * \return pair <odd averafe, even average>
+ */
+DPair averageLineLen(const Lyli::Calibration::LineMap& lines) {
+	double sumOdd = 0.0;
+	double sumEven = 0.0;
 
-	// compute average count of points in lines
-	double sum = 0.0;
-	for (const auto &line : lines) {
-		sum += line.second.size();
+	std::size_t iterations = lines.size() % 2 == 0 ? lines.size() : lines.size() - 1;
+	auto it = lines.begin();
+	for (std::size_t i = 0; i < iterations; ++i) {
+		sumOdd = (it++)->second.size();
+		sumEven = (it++)->second.size();
 	}
-	double avg = sum / lines.size();
+	if (lines.size() - iterations != 0) {
+		sumOdd = (it++)->second.size();
+	}
 
-	// remove the lines that are too short
-	LineMap filtered;
-	LinePtrList filteredList;
+	double countEven = iterations / 2.0;
+	double countOdd = countEven + (lines.size() - iterations);
+
+	return std::make_pair(sumOdd / countOdd, sumEven / countEven);
+}
+
+/**
+ * Filter out lines that are too short
+ *
+ * \param lines lines to filter
+ * \param avgLen average lengths of lines
+ * \param filteredMap OUT filtered map of lines
+ * \param filteredList OUT list of pointers to filtered lines
+ */
+int shortLineFilter(const Lyli::Calibration::LineMap& lines, DPair avgLen, Lyli::Calibration::LineMap &filteredMap, LinePtrList &filteredList) {
+	// use only one average to speedup the proces, both averages should be pretty close anyway
+	double avg = 0.5 * (avgLen.first + avgLen.second);
+
 	for (const auto &line : lines) {
 		if (std::abs(line.second.size() - avg) < 7) {
-			auto res = filtered.insert(line);
+			auto res = filteredMap.insert(line);
 			filteredList.push_back(& (res.first->second));
 		}
 	}
 
-#ifndef NDEBUG
-	stats.shortlines = lines.size() - filteredList.size();
-#endif
+	return lines.size() - filteredList.size();
+}
 
-	// now compute average distance between lenses for odd and even lines,
-	// it will be used to detect and add missing centroids
+DPair averageLensDist(const LinePtrList &filteredList) {
 	// the middle two lines are used to compute the average
-	std::size_t posOdd = !((filteredList.size() / 2) & 1u) ? (filteredList.size() / 2) : (filteredList.size() / 2) - 1;
-	const Line &lineOdd(* filteredList[posOdd]);
-	const Line &lineEven(* filteredList[posOdd + 1]);
+	std::size_t posOdd = (filteredList.size() / 2) & 1u ? (filteredList.size() / 2) - 1 : (filteredList.size() / 2);
+	const auto &lineOdd(* filteredList[posOdd]);
+	const auto &lineEven(* filteredList[posOdd + 1]);
 	double sumOdd = 0.0;
 	double sumEven = 0.0;
+
 	for (std::size_t i = 1; i < lineOdd.size(); ++i) {
 		sumOdd += lineOdd[i].y - lineOdd[i-1].y;
 	}
 	for (std::size_t i = 1; i < lineEven.size(); ++i) {
 		sumEven += lineEven[i].y - lineEven[i-1].y;
 	}
-	double avgOdd = sumOdd / (lineOdd.size() - 1.0);
-	double avgEven = sumEven / (lineEven.size() - 1.0);
+
+	return std::make_pair(sumOdd / (lineOdd.size() - 1.0), sumEven / (lineEven.size() - 1.0));
+}
+
+}
+
+namespace Lyli {
+namespace Calibration {
+
+LineMap LensFilter::filter(const LineMap& lines) {
+	// compute average count of points in lines
+	DPair avgLen = averageLineLen(lines);
+
+	// remove the lines that are too short
+	LineMap filtered;
+	LinePtrList filteredList;
+	shortLineFilter(lines, avgLen, filtered, filteredList);
+
+	// now compute average distance between lenses for odd and even lines,
+	// it will be used to detect and add missing centroids
+	DPair avgLensDist = averageLensDist(filteredList);
 
 	// find the average position of the boundary pixels
 	std::pair<double, double> boundsSumOdd = std::make_pair(0.0, 0.0);
@@ -99,65 +137,42 @@ LineMap LensFilter::filter(const LineMap& lines) {
 	// special handling of the first two lines
 	for (std::size_t i = 0; i < 2; ++i) {
 		Line &line = *filteredList[i];
-		double avgDiff = (i&1u) ? avgEven : avgOdd;
+		double avgDiff = (i&1u) ? avgLensDist.first : avgLensDist.second;
 		std::pair<double, double> bounds = (i&1u) ? boundsEven : boundsOdd;
 		if (std::abs(line.front().y - bounds.first) > 0.5*avgDiff) {
 			line.insert(line.begin(), cv::Point2f(line.front().x, bounds.first));
-#ifndef NDEBUG
-			stats.boundary++;
-#endif
 		}
 		if (std::abs(line.back().y - bounds.second) > 0.5*avgDiff) {
 			line.push_back(cv::Point2f(line.back().x, bounds.second));
-#ifndef NDEBUG
-			stats.boundary++;
-#endif
 		}
 	}
 	// intermediate lines
 	for (std::size_t i = 2; i < filteredList.size() - 2; ++i) {
 		Line &line = *filteredList[i];
-		double avgDiff = (i&1u) ? avgEven : avgOdd;
+		double avgDiff = (i&1u) ? avgLensDist.first : avgLensDist.second;
 		std::pair<double, double> bounds = (i&1u) ? boundsEven : boundsOdd;
 		if (std::abs(line.front().y - bounds.first) > 0.5*avgDiff) {
 			line.insert(line.begin(), cv::Point2f(line.front().x, bounds.first));
-#ifndef NDEBUG
-			stats.boundary++;
-#endif
 		}
 		if (std::abs(line.back().y - bounds.second) > 0.5*avgDiff) {
 			line.push_back(cv::Point2f(line.back().x, bounds.second));
-#ifndef NDEBUG
-			stats.boundary++;
-#endif
 		}
 	}
 	// special handling of the last two lines
 	for (std::size_t i = filteredList.size() - 2; i < filteredList.size(); ++i) {
 		Line &line = *filteredList[i];
-		double avgDiff = (i&1u) ? avgEven : avgOdd;
+		double avgDiff = (i&1u) ? avgLensDist.first : avgLensDist.second;
 		std::pair<double, double> bounds = (i&1u) ? boundsEven : boundsOdd;
 		if (std::abs(line.front().y - bounds.first) > 0.5*avgDiff) {
 			line.insert(line.begin(), cv::Point2f(line.front().x, bounds.first));
-#ifndef NDEBUG
-			stats.boundary++;
-#endif
 		}
 		if (std::abs(line.back().y - bounds.second) > 0.5*avgDiff) {
 			line.push_back(cv::Point2f(line.back().x, bounds.second));
-#ifndef NDEBUG
-			stats.boundary++;
-#endif
 		}
 	}
 
 	// now go through all lines and detect and missing centroids, possibly also remove outliers
 	// TODO
-
-#ifndef NDEBUG
-	std::cout << "removed " << stats.shortlines << " short lines" << std::endl;
-	std::cout << "added " << stats.boundary << " boundary points" << std::endl;
-#endif
 
 	return filtered;
 }
